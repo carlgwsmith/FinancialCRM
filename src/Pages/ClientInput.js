3fd9165b4893f0c055cb91cd0b990a72dfbdf322
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-11-12 12:56:52 -0600

    CRUD actions
    connect firestore and actions

--- src/Pages/ClientInput.js
+++ src/Pages/ClientInput.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {db} from '../firebase'
+import { addDoc, collection } from 'firebase/firestore'
+const ref = collection(db, "clients")
 
 const Clientinput = () => {
+  const [newFirst, setNewFirst] = useState('')
+  const [newLast, setNewLast] = useState('')
+  const [newAssets, setNewAssets] = useState(0)
+
+  const createClient = async () =>{
+    await addDoc(ref, {
+      firstName: newFirst,
+      lastName: newLast,
+      Assets: newAssets
+    })
+  }
   return (
     <div>
-      Add A Client Here
+    <h2>Add a client</h2>
+    <input placeholder="First Name" onChange={(event) => {setNewFirst(event.target.value)}}/>
+    <input placeholder="Last Name" onChange={(event) => {setNewLast(event.target.value)}}/>
+    <input placeholder="Assets" type="number" onChange={(event) => {setNewAssets(event.target.value)}}/>
+    <button onClick={createClient}>Add Client</button>
     </div>
   );
 }
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {db} from '../firebase'
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
 
 const Dashboard = () => {
+  const [clients, setClients] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const ref = collection(db, "clients")
+
+  const deleteClient = async (id) => {
+    const clientDoc = doc(db, "clients", id);
+    await deleteDoc(clientDoc)
+  }
+
+  useEffect(() => {
+    const getClients = async () =>{
+      const data = await getDocs(ref);
+      setClients(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      console.log(clients)
+    }
+    getClients()
+  }, [deleteClient]);
+
+  if (loading) {
+    return <div>
+      loading...
+    </div>;
+  }
   return (
     <div>
-      Dashboard
+      <h1>Client Dashboard</h1>
+      {clients.map((client) => (
+        <div key={client.id} style={{border: '1px solid #bababa', marginBottom:"10px"}}>
+          <p>{client.firstName} {client.lastName}</p>
+          <p>${client.Assets}</p>
+          <button onClick={()=> {deleteClient(client.id)}}>Delete Client</button>
+        </div>
+      ))}
     </div>
   );
 }
